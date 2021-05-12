a26edd2c76d2979fed20b0871a63a59d0b5949ae
Robinson Soares <soares.robinson@gmail.com>
2021-05-12 09:36:43 -0300

    Page Contact-Me

--- frontend/src/components/GlobalStyle.js
+++ frontend/src/components/GlobalStyle.js
@@ -6,58 +6,61 @@ const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
-
+    html{
+        @media (max-width: 1700px){
+            font-size: 75%;
+        }
+    }
     body{
         background: #1b1b1b;
+        font-family: 'Roboto', sans-serif;
         overflow-x: hidden;
         text-decoration: none;
     }
-
     button{
         font-weight: bold;
-        font-size: 1.1rem;
+        font-size: 1.1.rem;
         cursor: pointer;
         padding: 1rem 2rem;
+        border: 3px solid #416CD5;
         background: transparent;
         color: yellow;
-        border: 3px solid #416CD5;
         transition: all 0.5s ease;
-        &hover{
+        font-family: 'Inter', sans-serif;
+        &:hover{
             background-color: #23d997;
             color: white;
         }
     }
-
     h2{
-        font-weight:lighter;
-        font-size:4rem;
+        font-weight: lighter;
+        font-size: 4rem;
     }
-
     h3{
-        color:white;
+        color: white;
     }
-
     h4{
-        font-weight: bold;
+        font-weight:bold;
         font-size: 1.2rem;
         color: lightgrey;
     }
-
     a{
-        font-size: 1.1rem
+        font-size: 1.1.rem;
     }
-
     span{
         font-weight: lighter;
         color: #416CD5;
     }
-
     p{
         padding: 3rem 0rem;
         color: #ccc;
         font-size: 1.4rem;
         line-height: 150%;
     }
+    label{
+        color: black;
+        font-weight: bold;
+    }
 `;
 
-export default GlobalStyle
+export default GlobalStyle;
